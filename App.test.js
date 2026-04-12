8aae9c845b7cc951dcec0e5cffcd0be0bc3c983b
Rahul Dev <rahuldevbera098@gmail.com>
2026-04-12 15:41:13 +0530

    Update App.test.js

--- App.test.js
+++ App.test.js
@@ -1,8 +1,1 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders Ayurveda Guide header', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Ayurveda Guide/i);
-  expect(linkElement).toBeInTheDocument();
-});
